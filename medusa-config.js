04481ee17f7ecf4dd15a8d3dfa09b1c8da85732e
Martim Dornellas <martim.a.dornellas@gmail.com>
2023-12-30 11:01:19 +0000

    Update medusa-config.js to RENDER DATA

--- medusa-config.js
+++ medusa-config.js
@@ -29,9 +29,9 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+  "postgres://medserverdb_user:8McgbMvsrimup6vwukvAogx0mwFWoYmP@dpg-cm7urkmn7f5s73ce8cig-a/medserverdb";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = "redis://red-cm7ust6n7f5s73ce8fvg:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
